30c3e5d23af17b0006182e2a81a5c71f20dd00b9
Ajay <60919132+Ajay2521@users.noreply.github.com>
2024-10-22 11:29:05 +0530

    Update Social.jsx

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,14 +3,14 @@ import { GrInstagram, GrGithub, GrLinkedin } from "react-icons/gr";
 
 const Social = () => {
     return (
-       <div class="home__social">
-            <a href="https://www.linkedin.com/in/ajay-2521/" class="home__social-icon" target="_blank">
+       <div className="home__social">
+            <a href="https://www.linkedin.com/in/ajay-2521/" className="home__social-icon" target="_blank">
                 <GrLinkedin />
             </a>
-            <a href="https://github.com/Ajay2521" class="home__social-icon" target="_blank">
+            <a href="https://github.com/Ajay2521" className="home__social-icon" target="_blank">
                 <GrGithub />
             </a>
-             <a href="https://www.instagram.com/maayakavi/profilecard/" class="home__social-icon" target="_blank">
+             <a href="https://www.instagram.com/maayakavi/profilecard/" className="home__social-icon" target="_blank">
                 <GrInstagram />
             </a>
        </div>
