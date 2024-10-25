637f4fb550d676b850df93e09bd14cadff0b51bd
Ajay <60919132+Ajay2521@users.noreply.github.com>
2024-10-25 12:22:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import Header from './components/header/Header';
-import Home from './components/home/Home';
-import './App.css';
+import React from "react";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
+import Services from "./components/services/Services";
+import Qualification from "./components/qualification/Qualification";
+import "./App.css";
 
 function App() {
   return (
-   <>
-    <Header />
-    <main className='main'>
-      <Home />
-    </main>
-   </>
+    <>
+      <Header />
+      <main className="main">
+        <Home />
+        <About />
+        <Skills />
+        <Services />
+        <Qualification />
+      </main>
+    </>
   );
 }
 
