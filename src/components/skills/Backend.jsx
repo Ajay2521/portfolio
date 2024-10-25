97f39ff773b275284906b3fe6db9d0baac760c0e
Ajay <60919132+Ajay2521@users.noreply.github.com>
2024-10-25 12:20:07 +0530

    Update Backend.jsx

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -1,67 +1,66 @@
-import React from 'react';
+import React from "react";
 import { LuBadgeCheck } from "react-icons/lu";
 
 const Backend = () => {
-    return (
-        <div className="skills__content">
-            <h3 className="skills__title">Back-End Development</h3>
+  return (
+    <div className="skills__content">
+      <h3 className="skills__title">Back-End Development</h3>
 
-            <div className="skills__box">
-                <div className="skills__group">
-                    <div className="skills__data">
-                        <LuBadgeCheck className="skills__icon" />
-                        <div>
-                            <h3 className="skills__name">GO</h3>
-                            <span className="skills__level">Advance</span>
-                        </div>
-                    </div>
-
-                    <div className="skills__data">
-                        <LuBadgeCheck className="skills__icon" />
-                        <div>
-                            <h3 className="skills__name">Python</h3>
-                            <span className="skills__level">Intermediate</span>
-                        </div>
-                    </div>
-
-                    <div className="skills__data">
-                        <LuBadgeCheck className="skills__icon" />
-                        <div>
-                            <h3 className="skills__name">Node.js</h3>
-                            <span className="skills__level">Intermediate</span>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="skills__group">
-                    <div className="skills__data">
-                        <LuBadgeCheck className="skills__icon" />
-                        <div>
-                            <h3 className="skills__name">Postgres</h3>
-                            <span className="skills__level">Advance</span>
-                        </div>
-                    </div>
-
-                    <div className="skills__data">
-                        <LuBadgeCheck className="skills__icon" />
-                        <div>
-                            <h3 className="skills__name">MySql</h3>
-                            <span className="skills__level">Intermediate</span>
-                        </div>
-                    </div>
-
-                    <div className="skills__data">
-                        <LuBadgeCheck className="skills__icon" />
-                        <div>
-                            <h3 className="skills__name">MongoDB</h3>
-                            <span className="skills__level">Basic</span>
-                        </div>
-                    </div>
-                </div>
-
+      <div className="skills__box">
+        <div className="skills__group">
+          <div className="skills__data">
+            <LuBadgeCheck className="skills__icon" />
+            <div>
+              <h3 className="skills__name">GO</h3>
+              <span className="skills__level">Advance</span>
             </div>
+          </div>
+
+          <div className="skills__data">
+            <LuBadgeCheck className="skills__icon" />
+            <div>
+              <h3 className="skills__name">Python</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <LuBadgeCheck className="skills__icon" />
+            <div>
+              <h3 className="skills__name">Node.js</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
         </div>
-    );
-}
+
+        <div className="skills__group">
+          <div className="skills__data">
+            <LuBadgeCheck className="skills__icon" />
+            <div>
+              <h3 className="skills__name">Postgres</h3>
+              <span className="skills__level">Advance</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <LuBadgeCheck className="skills__icon" />
+            <div>
+              <h3 className="skills__name">MySql</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <LuBadgeCheck className="skills__icon" />
+            <div>
+              <h3 className="skills__name">MongoDB</h3>
+              <span className="skills__level">Basic</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Backend;
