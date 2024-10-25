410c78ceb051a6a5ee53de1ff23d487206102992
Ajay <60919132+Ajay2521@users.noreply.github.com>
2024-10-25 12:12:54 +0530

    Formatted Info.jsx

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -4,29 +4,28 @@ import { FaLaptopCode } from "react-icons/fa";
 import { GoTasklist } from "react-icons/go";
 import { BiSupport } from "react-icons/bi";
 
-
 const Info = () => {
-    return (
-        <div className="about__info grid">
-            <div className="about__box">
-                <FaLaptopCode className="about__icon" />
-                <h3 className="about__title">Experience</h3>
-                <span className="about__subtitle">2 Years & 10 Months</span>
-            </div>
+  return (
+    <div className="about__info grid">
+      <div className="about__box">
+        <FaLaptopCode className="about__icon" />
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">2 Years & 10 Months</span>
+      </div>
 
-            <div className="about__box">
-                <GoTasklist className="about__icon" />
-                <h3 className="about__title">Completed</h3>
-                <span className="about__subtitle">27+ Projects</span>
-            </div>
+      <div className="about__box">
+        <GoTasklist className="about__icon" />
+        <h3 className="about__title">Completed</h3>
+        <span className="about__subtitle">27+ Projects</span>
+      </div>
 
-            <div className="about__box">
-                <BiSupport className="about__icon" />
-                <h3 className="about__title">Support</h3>
-                <span className="about__subtitle">Online 24/7</span>
-            </div>
-        </div>
-    );
-}
+      <div className="about__box">
+        <BiSupport className="about__icon" />
+        <h3 className="about__title">Support</h3>
+        <span className="about__subtitle">Online 24/7</span>
+      </div>
+    </div>
+  );
+};
 
 export default Info;
