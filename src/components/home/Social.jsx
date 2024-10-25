85474bb905dd852baec48ff050db9b996f7bdb1d
Ajay <60919132+Ajay2521@users.noreply.github.com>
2024-10-25 12:17:49 +0530

    Update Social.jsx

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -2,19 +2,31 @@ import React from "react";
 import { GrInstagram, GrGithub, GrLinkedin } from "react-icons/gr";
 
 const Social = () => {
-    return (
-       <div className="home__social">
-            <a href="https://www.linkedin.com/in/ajay-2521/" className="home__social-icon" target="_blank">
-                <GrLinkedin />
-            </a>
-            <a href="https://github.com/Ajay2521" className="home__social-icon" target="_blank">
-                <GrGithub />
-            </a>
-             <a href="https://www.instagram.com/maayakavi/profilecard/" className="home__social-icon" target="_blank">
-                <GrInstagram />
-            </a>
-       </div>
-    )
-} 
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.linkedin.com/in/ajay-2521/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <GrLinkedin />
+      </a>
+      <a
+        href="https://github.com/Ajay2521"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <GrGithub />
+      </a>
+      <a
+        href="https://www.instagram.com/maayakavi/profilecard/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <GrInstagram />
+      </a>
+    </div>
+  );
+};
 
 export default Social;
