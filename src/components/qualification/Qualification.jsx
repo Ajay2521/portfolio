b470c759bbe2c7b0ff7ec28f73f82116a25b563c
Ajay <60919132+Ajay2521@users.noreply.github.com>
2024-10-25 12:18:41 +0530

    Update Qualification.jsx

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -1,162 +1,117 @@
-import React, { useState } from "react";
-import "./qualification.css";
-import { HiOutlineAcademicCap } from "react-icons/hi";
-import { MdOutlineWorkHistory } from "react-icons/md";
-import { LuCalendarCheck2 } from "react-icons/lu";
+.qualification__container {
+  max-width: 768px;
+}
 
-const Qualification = () => {
-  const [toggleState, setToggleState] = useState(1);
+.qualification__tabs {
+  display: flex;
+  justify-content: center;
+  margin-bottom: var(--mb-2);
+}
 
-  const toggleTab = (index) => {
-    setToggleState(index);
-  };
+.qualification__button {
+  font-size: var(--h3-font-size);
+  font-weight: var(--font-medium);
+  color: var(--title-color);
+  margin: 0 var(--mb-1);
+  cursor: pointer;
+}
 
-  return (
-    <section className="qualification section">
-      <h2 className="section__title">Qualification</h2>
-      <span className="section__subtitle">My Journey</span>
+.qualification__button:hover {
+  color: var(--title-color-dark);
+}
 
-      <div className="qualification__container container">
-        <div className="qualification__tabs">
-          <div
-            className={
-              toggleState === 1
-                ? "qualification__button  qualification__active  button--flex"
-                : "qualification__button button--flex"
-            }
-            onClick={() => toggleTab(1)}
-          >
-            <MdOutlineWorkHistory className="qualification__icon" />
-            Experience
-          </div>
-          <div
-            className={
-              toggleState === 2
-                ? "qualification__button  qualification__active  button--flex"
-                : "qualification__button button--flex"
-            }
-            onClick={() => toggleTab(2)}
-          >
-            <HiOutlineAcademicCap className="qualification__icon" />
-            Education
-          </div>
-        </div>
+.qualification__icon {
+  font-size: 1.8rem;
+  margin-right: var(--mb-0-25);
+}
 
-        <div className="qualification__sections">
-          <div
-            className={
-              toggleState === 1
-                ? "qualification__content  qualification__content-active"
-                : "qualification__content"
-            }
-          >
-            <div className="qualification__data">
-              <div>
-                <h3 className="qualification__title">Software Engineer</h3>
-                <span className="qualification__subtitle">
-                  Funding Societies | Modalku
-                </span>
-                <div className="qualification__calendar">
-                  <LuCalendarCheck2 className="qualification__calendar-icon" />
-                  2022 - Present
-                </div>
-              </div>
+.qualification__active {
+  color: var(--title-color-dark);
+}
 
-              <div>
-                <span className="qualification__rounder"></span>
-                <span className="qualification__line"></span>
-              </div>
-            </div>
+.qualification__sections {
+  display: grid;
+  grid-template-columns: 0.5fr;
+  justify-content: center;
+}
 
-            <div className="qualification__data">
-              <div></div>
+.qualification__content {
+  display: none;
+}
 
-              <div>
-                <span className="qualification__rounder"></span>
-                <span className="qualification__line"></span>
-              </div>
-              <div>
-                <h3 className="qualification__title">
-                  Software Engineer Intern
-                </h3>
-                <span className="qualification__subtitle">
-                  Funding Societies | Modalku
-                </span>
-                <div className="qualification__calendar">
-                  <LuCalendarCheck2 className="qualification__calendar-icon" />
-                  Jan 2022 - Jul 2022
-                </div>
-              </div>
-            </div>
+.qualification__content-active {
+  display: block;
+}
 
-            <div className="qualification__data">
-              <div>
-                <h3 className="qualification__title">Summer Intern</h3>
-                <span className="qualification__subtitle">Zoho Corp</span>
-                <div className="qualification__calendar">
-                  <LuCalendarCheck2 className="qualification__calendar-icon" />
-                  May 2021 - Jun 2021
-                </div>
-              </div>
+.qualification__data {
+  display: grid;
+  grid-template-columns: 1fr max-content 1fr;
+  column-gap: 1.5rem;
+}
 
-              <div>
-                <span className="qualification__rounder"></span>
-                <span className="qualification__line"></span>
-              </div>
-            </div>
-          </div>
-          <div
-            className={
-              toggleState === 2
-                ? "qualification__content  qualification__content-active"
-                : "qualification__content"
-            }
-          >
-            <div className="qualification__data">
-              <div>
-                <h3 className="qualification__title">
-                  Bachelor Of Information Technology
-                </h3>
-                <span className="qualification__subtitle">
-                  Coimbatore Institute of Technology
-                </span>
-                <div className="qualification__calendar">
-                  <LuCalendarCheck2 className="qualification__calendar-icon" />
-                  2018 - 2022
-                </div>
-              </div>
+.qualification__title {
+  font-size: var(--normal-font-size);
+  font-weight: var(--font-medium);
+}
 
-              <div>
-                <span className="qualification__rounder"></span>
-                <span className="qualification__line"></span>
-              </div>
-            </div>
+.qualification__subtitle {
+  display: inline-block;
+  font-size: var(--smaller-font-size);
+  margin-bottom: var(--mb-1);
+}
 
-            <div className="qualification__data">
-              <div></div>
+.qualification__calendar {
+  font-size: var(--small-font-size);
+}
 
-              <div>
-                <span className="qualification__rounder"></span>
-                <span className="qualification__line"></span>
-              </div>
-              <div>
-                <h3 className="qualification__title">
-                  Higher Secondary Education
-                </h3>
-                <span className="qualification__subtitle">
-                  St. Joseph's Matriculation Higher Secondary School
-                </span>
-                <div className="qualification__calendar">
-                  <LuCalendarCheck2 className="qualification__calendar-icon" />
-                  2017 - 2018
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
+.qualification__calendar-icon {
+  margin-top: var(--mb-0-25);
+  margin-right: var(--mb-0-25);
+}
 
-export default Qualification;
+.qualification__rounder {
+  display: inline-block;
+  width: 13px;
+  height: 13px;
+  background-color: var(--text-color);
+  border-radius: 50%;
+}
+
+.qualification__line {
+  display: block;
+  width: 1px;
+  height: 100%;
+  background-color: var(--text-color);
+  transform: translate(6px, -7px);
+}
+
+/* BREAK POINTS */
+@media screen and (max-width: 992px) {
+  .qualification__container {
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .qualification__container {
+    margin-left: var(--mb-1-5);
+    margin-right: var(--mb-1-5);
+  }
+}
+
+@media screen and (max-width: 576px) {
+  .qualification__sections {
+    grid-template-columns: initial;
+  }
+
+  .qualification__button {
+    margin: 0 var(--mb-0-75);
+  }
+}
+
+@media screen and (max-width: 350px) {
+  .qualification__data {
+    gap: 0.5rem;
+  }
+}
